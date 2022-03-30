d32600ff70e6d81d7e6b64b15ed820487d8954a4
Alina <alinazulfiqar@gmail.com>
2022-03-30 14:02:37 -0700

    changes

--- index.js
+++ index.js
@@ -264,8 +264,8 @@ app.get("/victorian", async (req, res) => {
 });
 
 
-app.get("*", (req,res)=>{
-  res.sendFile(path.join(__dirname, "/client/build/index.html"))
+app.get("/*", (req,res)=>{
+  res.sendFile(path.join(__dirname, "client/build/index.html"))
 })
 // listen
 
