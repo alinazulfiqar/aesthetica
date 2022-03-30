5150ad083840e1cd81f9fd2a8604aa0ea060d211
Alina <alinazulfiqar@gmail.com>
2022-03-30 16:06:39 -0700

    changes

--- index.js
+++ index.js
@@ -15,9 +15,17 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-if(process.env.NODE_ENV === "production"){
-app.use(express.static(path.join(__dirname, "client/build")));
-}
+if (process.env.NODE_ENV) {
+  //static folder add
+   app.use(express.static('app/client/build'));
+   app.get("*", function (req, res) {
+   res.sendFile(path.resolve(__dirname , "app/client/build", "index.html"));
+    });
+  }
+
+// if(process.env.NODE_ENV === "production"){
+// app.use(express.static(path.join(__dirname, "client/build")));
+// }
 console.log(path.join(__dirname, "client/build"));
 // **routes**
 // post to db
@@ -265,9 +273,9 @@ app.get("/victorian", async (req, res) => {
 });
 
 
-app.get("*", (req,res)=>{
-  res.sendFile(path.join(__dirname+ "client/build", "index.html"))
-})
+// app.get("*", (req,res)=>{
+//   res.sendFile(path.join(__dirname+ "client/build", "index.html"))
+// })
 // listen
 
 app.listen(PORT, () => {
