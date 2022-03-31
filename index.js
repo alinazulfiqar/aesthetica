55f90e8235a1dd1ed1c34701b657be90a20023f1
Alina <alinazulfiqar@gmail.com>
2022-03-31 00:56:58 -0700

    f

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(express.json());
 // if(process.env.NODE_ENV === "production"){
 app.use(express.static(path.join(__dirname, "client/build")));
 
+
 console.log(path.join(__dirname, "client/build"));
 // **routes**
 // post to db
